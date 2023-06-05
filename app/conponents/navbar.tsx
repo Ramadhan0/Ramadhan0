b2ae32621c517275bd84bc57197daf1531da36f8
Rama <rama@Ramas-MacBook-Air.local>
2023-06-05 10:07:14 +0200

    build homepage

---
+++ app/conponents/navbar.tsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => (
+    <div className='bg-slate-900 w-100 h-16 flex justify-between px-36'>
+        <ul className='mt-6'><li>HOME</li></ul>
+        <ul className='flex mt-6'>
+            <li className='px-6'>WORK</li>
+            <li className='px-6'>ABOUT</li>
+            <li className='px-6'>CONTACT</li>
+        </ul>
+    </div>
+)
+
+export default Navbar
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+const Home = () => (
+  <div className='px-36 place-content-center '>
+    <p className='text-slate-300 w-2/5 text-3xl pt-48'>
+      Hello, <br />
+      I'm Ramadhan Nsengiyumva,  <br />
+      Full-stack software developer/DevOps <br />
+      Based in Kigali, Rwanda
+    </p>
+
+    <p className='text-slate-500 w-1/2 text-md pt-20'>
+      4+ years of experience, and I specialize in developing robust web applications
+      using a wide range of front-end and back-end technologies. My passion lies in creating seamless
+      user experiences and delivering high-quality code.
+    </p>
+  </div>
+)
+
+export default Home
