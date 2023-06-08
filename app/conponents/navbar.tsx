d8e2732f292451a2766d6e5230f7151575a0050e
Rama <rama@Ramas-MacBook-Air.local>
2023-06-08 14:56:18 +0200

    work

--- app/conponents/navbar.tsx
+++ app/conponents/navbar.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 
 const Navbar = () => (
     <div className='bg-slate-900 w-100 h-16 flex justify-between px-36'>
-        <ul className='mt-6'><li>HOME</li></ul>
+        <ul className='mt-6'><li><Link href="/">HOME</Link></li></ul>
         <ul className='flex mt-6'>
-            <li className='px-6'>WORK</li>
-            <li className='px-6'>ABOUT</li>
-            <li className='px-6'>CONTACT</li>
+            <li className='px-6'> <Link href="/work">WORK</Link> </li>
+            <li className='px-6'><Link href="/about">ABOUT</Link></li>
+            <li className='px-6'><Link href="/contact">CONTACT</Link></li>
         </ul>
     </div>
 )
